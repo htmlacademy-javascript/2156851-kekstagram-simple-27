fc561041a6a86db9fd0650d191d5a08f99269257
Ismail <ismail.salavatov@internet.ru>
2022-11-23 11:09:24 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import { renderPictures } from './pictures.js';
 import './forms.js';
-import { getData, sendData } from './api.js';
+import { getData } from './api.js';
 import { renderGetErrorMessage } from './error.js';
 getData((data) => { renderPictures(data) }, renderGetErrorMessage);
